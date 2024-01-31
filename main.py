abcb1e995b36a944c2c27e941bb950bacdbe1833
Nameless8243 <119530613+Nameless8243@users.noreply.github.com>
2024-01-31 17:23:39 +0100

    Finished

--- main.py
+++ main.py
@@ -4,26 +4,28 @@ from pathlib import Path
 import glob
 import pyarrow
 
+# Create a list of text filepaths
+filepaths = glob.glob("files/*.txt")
+
+# Create one PDF file
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-filepaths = glob.glob("texts/*.txt")
-
+# Go through each text file
 for filepath in filepaths:
-    df = pd.read_fwf(filepath)
-    print(filepath)                                  #
-
+    # Add a page to the PDF document for each text file
     pdf.add_page()
+
+    # Get the filename without the extension
+    # and convert it to title case (e.g. Cat)
     filename = Path(filepath).stem
-    animal_name = filename.split("-")[0]
-    print(animal_name)                              #
+    animal_name = filename.title()
 
     # Set the header
-
-    pdf.set_font(family="Times", style="B", size=24)
-    pdf.cell(w=0, h=12, txt=animal_name, align="L", ln=1)
+    pdf.set_font(family="Times", style="B", size=16)
+    pdf.cell(w=50, h=8, txt=animal_name, ln=1)
 
     # Set the text
+    ######################
 
-
-
+# Produce the PFD
 pdf.output("output.pdf")
